5fe79c6bca23652b2f02d8ee62ab0abe3e0fdee6
José Saavedra <jsaavedv@cl.ibm.com>
2023-04-11 00:25:36 -0400

    Se realizan ultimos ajustes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
+const moment = require('moment-timezone');
 const PORT = 3000;
+const TIMEZONE = 'America/Santiago';
+
 app.use(express.json())
 
 app.post('/', function (request, response) {
@@ -11,13 +13,9 @@ app.post('/', function (request, response) {
     const dates = body.dates;
     data.dates = new Array();
     dates.forEach(datestr => {
-      console.log(stringToDate(datestr));
       data.dates.push(stringToDate(datestr).toString());
     });
-
-
-    
-    data.nowDate = (new Date()).toString();
+    data.nowDate = (dateToTimezone(new Date(), TIMEZONE)).toUTCString();
     response.status(200).send({code: 200, resp: data});
   } catch (e) {
     response.status(500).send({code: 0, resp: e.message});
@@ -26,6 +24,16 @@ app.post('/', function (request, response) {
 
 function stringToDate(datestring) {
   let date = new Date(datestring);
+  const timezoneServer = Number.parseInt(moment(date).format('Z'));
+  date.setHours(date.getHours() + (timezoneServer * -1));
+  return date;
+}
+
+function dateToTimezone (date, timezone) {
+  const timezoneCL = Number.parseInt(moment(date).tz(timezone).format('Z'));
+  const timezoneServer = Number.parseInt(moment(date).format('Z'));
+  const timezoneApply = timezoneCL + (timezoneServer * -1);
+  date.setHours(date.getHours() + (timezoneApply));
   return date;
 }
 
